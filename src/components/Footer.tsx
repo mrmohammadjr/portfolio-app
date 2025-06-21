57d94c1d0084acaa2a292a91438ae579c2152125
mrmohammadjr <javadrma.2017@gmail.com>
2025-06-21 19:32:10 +0330

    upup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,13 +22,51 @@ const Footer: React.FC<Props> = ({ setItem }) => {
           theme === "light" ? "border-none" : "border-2 border-white"
         }`}
       />
+      <div className="mt-7 border-b-2 w-fit flex justify-start">
+        <a href="mailto:mr.mohammadjavadrasooli@gmail.com" className="">
+          <i
+            className={`${
+              theme === "light" ? "text-[#161616]" : "text-[#FDFFFC]"
+            } pi pi-envelope sm:p-3 lg:text-3xl sm:text-3xl cursor-pointer`}
+          ></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mohammad-javad-rasooli-282505260?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          className=""
+        >
+          <i
+            className={`${
+              theme === "light" ? "text-[#161616]" : "text-[#FDFFFC]"
+            } pi pi-linkedin sm:p-3 lg:text-3xl sm:text-3xl cursor-pointer`}
+          ></i>
+        </a>
+        <a
+          href="https://www.instagram.com/mr_mohammadjr?utm_source=qr&igsh=eGFteGFjM3UyM2pn"
+          className=""
+        >
+          <i
+            className={`${
+              theme === "light" ? "text-[#161616]" : "text-[#FDFFFC]"
+            } pi pi-instagram sm:p-3 lg:text-3xl sm:text-3xl cursor-pointer`}
+          ></i>
+        </a>
+        <a href="https://github.com/mrmohammadjr" className="">
+          <i
+            className={`${
+              theme === "light" ? "text-[#161616]" : "text-[#FDFFFC]"
+            } pi pi-github  sm:p-3 lg:text-3xl sm:text-3xl cursor-pointer`}
+          ></i>
+        </a>
+      </div>
       <div className="flex justify-start items-center mt-5">
         <h1 className=" lg:mt-0 mr-5 sm:mt-6 lg:text-2xl sm:text-2xl font-bold">
           Made With
         </h1>
         <Libraries />
       </div>
-       <p className="lg:text-xl sm:text-md mt-6 text-center">Designed & Created by Mohammad Javad Rasooli</p>
+      <p className="lg:text-md sm:text-md mt-8 text-gray-500 text-center">
+        Designed & Created by Mohammad Javad Rasooli
+      </p>
     </div>
   );
 };
